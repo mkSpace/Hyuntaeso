3222aa089b2a51520ebd0b5b381f9cfa1f2854b7
Jaemin.Park <once29@naver.com>
2022-06-17 17:00:36 +0900

    Add testing spam data

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,25 +1,43 @@
 import candidate.CandidateSearcher
 import dictionary.AssetDataDictionary
+import excel.ExcelReader
 import hangeul.Hangeul
 
 fun main() {
-    val sample = "안녕하세요 제 이름은 박재민 입니다. 우리 모두 사이 좋게 지내요! 2.참 고 : 어떻게 1인 무점포 창업 으로 수익이 발생하는지 설명회 당일 확실히 보여드리겠습니다. 3.날 짜 : 11 월 16일 금요일 오후 2 시 ~ 또는 29일 목요일 오후 2시 중 선택   [자세히 보기]"
-        .replace(".", "")
-        .replace("!", "")
-        .replace("?", "")
-    println(sample)
-    println(Hangeul.spreadHangeulString(sample))
+    val dataList =
+        ExcelReader.xlsToDataList("/Users/jaemin/Desktop/WorkSpace/etc/Hyuntaeso/src/main/resources/spam_data_test5.xlsx")
     val dictionary = AssetDataDictionary()
-    val wordList = mutableListOf<String>()
-    sample.split(" ").forEach { word ->
+    val wordGroupList =
+        dataList.groupBy { it.type }.map { it.key to it.value.map { it.subtractNouns(dictionary) }.flatten() }
+    val spamList = wordGroupList.first().second.groupBy { it }.map { it.key to it.value.size }
+    val hamList = wordGroupList.last().second.groupBy { it }.map { it.key to it.value.size }
+    val hamCount = hamList.sumOf { it.second }
+    val spamCount = spamList.sumOf { it.second }
+    println("hamList: $hamList\nspamList: $spamList")
+    println("hamCount: $hamCount, spamCount: $spamCount")
+    val test = "오늘 저녁 같이 먹자 내일은 시간이 안돼 답장 빨리해줘 제발"
+    val testWordList = mutableListOf<String>()
+    test.split(" ").forEach { word ->
         val searcher = CandidateSearcher(dictionary, word, Hangeul.spreadHangeulString(word))
         searcher.search()
         val allCandidates = searcher.getCandidateArrayList().flatten()
             .map { it.componentList }
-            .map { components -> components.filter { it.getTypeName() == "명사" } }
+            .map { components -> components.filter { it.getTypeName() == "명사" && it.getSource().length > 1 } }
             .flatten()
             .map { it.getSource() }
-        allCandidates.firstOrNull()?.let { wordList.add(it) }
+        allCandidates.firstOrNull()?.let { testWordList.add(it) }
     }
-    println(wordList)
+    println("test word: $testWordList")
+    var spamProbability = 1.00
+    var hamProbability = 1.00
+    testWordList.forEach { testWord ->
+        println("HAM!! : ${hamList.find { it.first == testWord }}")
+        println("SPAM!! : ${spamList.find { it.first == testWord }}")
+        hamProbability *= (((hamList.find { it.first == testWord }?.second?.toDouble()
+            ?: 0.005)) / hamCount.toDouble())
+        spamProbability *= (((spamList.find { it.first == testWord }?.second?.toDouble()
+            ?: 0.005)) / spamCount.toDouble())
+    }
+    println("spamProb : $spamProbability, hamProb: $hamProbability")
+    println(if (spamProbability > hamProbability) "SPAM" else "HAM")
 }
